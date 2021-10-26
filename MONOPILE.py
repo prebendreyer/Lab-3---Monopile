cefd765e0ab814aee1b501e7a3a4cd75a17e7825
prebendreyer <91905799+prebendreyer@users.noreply.github.com>
2021-10-26 15:50:22 +0200

    Add files via upload

---
+++ MONOPILE.py
@@ -0,0 +1,25 @@
+import catmanreader as cr
+import pandas as pd
+from matplotlib import pyplot as plt
+import numpy as np
+
+
+inputfiles = ['decay_1.bin', 'iw1_1.bin', 'rw3_1.bin', 'rw3_2.bin']
+
+# Creating a dictionary of dataframes for each run
+df = {}
+
+
+# Looping through and decoding each binary file, creating dataframes and storing them in dictionary, d.
+for i in inputfiles:
+    binary = cr.import_catman_binary(i)
+    data, keys = binary.get_data()
+    data = pd.DataFrame(data)
+    data.columns = keys
+    data.columns.values[0] = 't'
+    data.columns.map(str)
+    df[i[:-4]] = data
+
+decay = df['decay_1']
+
+print(decay.columns)
